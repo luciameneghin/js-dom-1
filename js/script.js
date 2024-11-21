492d094225306d3518fb4d361a5736a0038b960e
luciameneghin <lucia.meneghin00@gmail.com>
2024-11-21 15:34:23 +0100

    feat: add html <div> in js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@
 // const offBulb = document.querySelector('.bulb img')
 // console.log(offBulb.src);
 // offBulb.src = pathImage;
+
+// fase di preparazione
+const button = document.getElementById('switch')
+console.log(button.innerHTML);
+
+const bulbWhite = document.getElementById('bulbOff');
+console.log(bulbWhite.querySelector('img'));
+
+const bulbYellow = document.getElementById('bulbOn');
+console.log(bulbYellow.querySelector('img'));
+
+
+// const bulbSwitch = ()
